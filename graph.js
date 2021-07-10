7f6066d3d9be38a4279bee731cc9026bf2a78af5
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-07-10 11:53:29 -0400

    Update graph.js

--- graph.js
+++ graph.js
@@ -31,16 +31,50 @@ class Graph {
   }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    for (let node of this.nodes) {
+      if (node.adjacent.has(vertex)) {
+        node.adjacent.delete(vertex);
+         }
+         }
+  this.nodes.delete(vertex);
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    // TODO: implement depth first search
+    const visited = new Set();
+    const result = [];
+
+    function traverse(vertex){
+    //base case
+    if (!vertex){
+      return null;
+    }
+    //visit nodes
+      visited.add(vertex);
+      result.push(vertex.value);
+    
+    //visit neighbors 
+      for (let neighbor of vertex.adjacent) {
+        if (!visited.has(neighbor)) {
+          traverse(neighbor);
+        }
+      }
+    }
+      
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    //create an queue
+  }
 }
 
 module.exports = {Graph, Node}
